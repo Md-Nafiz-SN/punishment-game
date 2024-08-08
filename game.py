649daa0fcf63e9114c7ed0f68e98c1b4f0500f9b
Ex0tic <exoticbrine@gmail.com>
2024-08-08 21:53:29 +0600

    Add files via upload

---
+++ game.py
@@ -0,0 +1,24 @@
+print ("it's a game.if you fail this game you get punishment.")
+print ("question?")
+print (" user = __when I search exotic in google, it give me error__")
+print ("password = __mobile-lock screen ---- ")
+user = input("enter user name: ")
+password = input("enter password: ")
+
+if user == "ex0tic":
+    print ("weldone")
+
+else:
+        user = 1
+
+        while user <= 10:
+               print ("(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)(*_*)")
+if password == "1234":
+      print ("weldone")
+
+else:
+
+         password = 1
+
+         while password <= 10:
+                       print ("110010010100100100100101100010010100101010010010010110101010101100100110100100100100010100101010100101001000100100010010010010010100101010100010010110010010101010010101010010100100010100010010101000101001001000101000100100")
